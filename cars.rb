827f1715cb279aa3a936b23760710112b9d26de2
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-25 14:03:23 +0000

    automatically backed up by learn

---
+++ cars.rb
@@ -0,0 +1,23 @@
+
+class Cars
+  
+  def initialize (model,make,year)
+    @model = model
+    @make = make
+    @year = year 
+    
+    puts "i love my #{year} #{model}, it is #{make}"
+  end
+  def stereo = (car_stereo)
+    @stereo = car_stereo
+  end
+  def cam = (backup_cam)
+    
+end
+
+toyota = Cars.new ("camry", "toyota","1997")
+prius = Cars.new ("prius", "toyota", "2011")
+ 
+ puts " the car has #{prius_stereo}"
+
+
